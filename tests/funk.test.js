c357eee9f15f7a160402bfe3bf0cd36927bb5dbe
Pau <masylum@gmail.com>
2011-09-08 11:08:25 -0700

    fixing annoying bug

--- tests/funk.test.js
+++ tests/funk.test.js
@@ -3,136 +3,8 @@ var testosterone = require('testosterone')(),
     fs = require('fs');
 
 testosterone
-  .before(function () {
-  })
 
   .add('`sync` parallel', function (done) {
-    var funk = require('./../lib/funk')();
-
-    funk.set('results', []);
-    funk.separator = '';
-
-    funk.add(function () {
-      this.results.push('foo');
-    })();
-
-    funk.add(function () {
-      this.results.push('bar');
-    })();
-
-    funk.add(function () {
-      this.separator = ',';
-    })();
-
-    funk.parallel(function () {
-      assert.equal(this.results.join(this.separator), 'foo,bar');
-      done();
-    });
-  })
-
-  .add('`sync` serial', function (done) {
-    var funk = require('./../lib/funk')();
-
-    funk.set('values', []);
-
-    funk.add(function () {
-      this.values.push('foo');
-    })();
-
-    funk.add(function () {
-      this.values.push('bar');
-    })();
-
-    funk.add(function () {
-      this.separator = ',';
-    })();
-
-    funk.serial(function () {
-      assert.equal(this.values.join(this.separator), 'foo,bar');
-      done();
-    });
-  })
-
-  .add('`async` parallel', function (done) {
-    var funk = require('./../')();
-
-    funk.set('results', []);
-
-    fs.readFile(__dirname + "/data/foo.txt", 'utf-8', funk.add(function (er, data) {
-      this.results.push(data);
-    }));
-
-    fs.readFile(__dirname + "/data/bar.txt", 'utf-8', funk.add(function (er, data) {
-      this.results.push(data);
-    }));
-
-    funk.parallel(function () {
-      assert.equal(this.results.length, 2);
-      assert.notEqual(this.results.indexOf('foo\n'), -1);
-      assert.notEqual(this.results.indexOf('bar\n'), -1);
-      done();
-    });
-  })
-
-  .add('`async` nothing', function (done) {
-    var funk = require('./../')();
-
-    funk.results = [];
-
-    fs.readFile(__dirname + "/data/foo.txt", 'utf-8', funk.nothing());
-    fs.readFile(__dirname + "/data/bar.txt", 'utf-8', funk.nothing());
-
-    funk.parallel(function () {
-      assert.deepEqual(this, {});
-      done();
-    });
-  })
-
-  .add('`async` result', function (done) {
-    var funk = require('./../')();
-
-    funk.results = [];
-
-    fs.readFile(__dirname + "/data/foo.txt", 'utf-8', funk.result('file1'));
-    fs.readFile(__dirname + "/data/bar.txt", 'utf-8', funk.result('file2'));
-
-    funk.parallel(function () {
-      assert.equal(this.file1, 'foo\n');
-      assert.equal(this.file2, 'bar\n');
-      done();
-    });
-  })
-
-  .add('`async` serial', function (done) {
-    var funk = require('./../')(),
-        order = 0;
-
-    funk.results = [];
-
-    setTimeout(funk.add(function () {
-      order += 1;
-      funk.set('order_first', order);
-    }), 200);
-
-    fs.readFile(__dirname + "/data/foo.txt", 'utf-8', funk.add(function () {
-      order += 1;
-      funk.set('order_foo', order);
-    }));
-
-    fs.readFile(__dirname + "/data/bar.txt", 'utf-8', funk.add(function () {
-      order += 1;
-      funk.set('order_bar', order);
-    }));
-
-    funk.serial(function () {
-      assert.equal(this.order_first, 1);
-      assert.equal(this.order_foo, 2);
-      assert.equal(this.order_bar, 3);
-      done();
-    });
-  })
-
-  .add('`new API`, `sync` parallel', function (done) {
     var funk = require('./../lib/funk')('parallel');
 
     funk.set('results', []);
@@ -156,7 +28,7 @@ testosterone
     });
   })
 
-  .add('`new API`, `sync` serial', function (done) {
+  .add('`sync` serial', function (done) {
     var funk = require('./../lib/funk')('serial');
 
     funk.set('values', []);
@@ -179,7 +51,7 @@ testosterone
     });
   })
 
-  .add('`new API`, `async` parallel', function (done) {
+  .add('`async` parallel', function (done) {
     var funk = require('./../')('parallel');
 
     funk.set('results', []);
@@ -200,7 +72,7 @@ testosterone
     });
   })
 
-  .add('`new API`, `async` serial', function (done) {
+  .add('`async` serial', function (done) {
     var funk = require('./../')('serial'),
         order = 0;
 
@@ -229,7 +101,7 @@ testosterone
     });
   })
 
-  .add('`new API`, `error callback`', function (done) {
+  .add('`error callback`', function (done) {
     var funk = require('./../')('serial'),
         order = 0;
 
@@ -256,7 +128,7 @@ testosterone
     });
   })
 
-  .add('`new API`, `error callback` callbackless', function (done) {
+  .add('`error callback` callbackless', function (done) {
     var funk = require('./../')('serial'),
         order = 0;
 
